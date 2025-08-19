16660f8234fe6f85e1dadad7f16ac15f60bb07ee
wongyuzhen <wongyuzhenn@gmail.com>
2025-08-19 20:21:44 +0800

    Improve Jane chatbot so that it echos commands entered by user and exits with command bye

--- src/main/java/Jane.java
+++ src/main/java/Jane.java
@@ -1,11 +1,33 @@
+import java.util.Scanner;
+
 public class Jane {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String greeting = "____________________________________________________________\n" +
                 "Hello! I'm Jane\n" +
                 "What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                "Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\n";
         System.out.println(greeting);
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                String bye = "____________________________________________________________\n" +
+                        "Bye. Hope to see you again soon!\n" +
+                        "____________________________________________________________\n";
+                System.out.println(bye);
+                break;
+            }
+
+            String text = "____________________________________________________________\n" +
+                    input +
+                    "\n" +
+                    "____________________________________________________________\n";
+            System.out.println(text);
+        }
+
+        scanner.close();
     }
 }
