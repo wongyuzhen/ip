ee1b094634b118a0f934feecdbf5d7ed3f645f81
wongyuzhen <wongyuzhenn@gmail.com>
2025-08-20 00:28:51 +0800

    Add support for 3 types of tasks - ToDos, Deadlines and Events

--- src/main/java/Jane.java
+++ src/main/java/Jane.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Jane {
@@ -11,7 +12,7 @@ public class Jane {
         System.out.println(greeting);
 
         int count = 0;
-        Task list[] = new Task[100];
+        Task[] list = new Task[100];
 
         while (true) {
             String input = scanner.nextLine();
@@ -26,11 +27,11 @@ public class Jane {
             }
 
             if (input.equals("list")) {
-                System.out.println("____________________________________________________________\n");
+                System.out.println("____________________________________________________________\n"
+                    + "Here are the tasks in your list:\n");
                 for (int i = 0; i < 100; i++) {
                     if (list[i] != null) {
-                        System.out.println((i + 1) + ". [" + list[i].getStatusIcon() + "] " +
-                                list[i].getDescription() + "\n");
+                        System.out.println((i + 1) + ". " + list[i].toString() + "\n");
                     }
                 }
                 System.out.println("____________________________________________________________\n");
@@ -51,15 +52,47 @@ public class Jane {
                 System.out.println(taskLine + "\n");
                 System.out.println("____________________________________________________________\n");
             } else {
-                list[count] = new Task(input);
-                count++;
+                String[] inputArgs = input.split("\\s?/");
+                String[] mainCmd = inputArgs[0].split("\\s+", 2);
+                HashMap<String, String> flags = new HashMap<String, String>();
+                for (int i = 1; i < inputArgs.length; ++i) {
+                    String[] flagParts = inputArgs[i].split("\\s+", 2);
+                    flags.put(flagParts[0], flagParts[1]);
+                }
+                TaskType type = TaskType.valueOf(mainCmd[0].toUpperCase());
+                switch (type) {
+                    case TODO:
+                        // assert mainCmd.length == 1;
+                        list[count] = new Task(mainCmd[1]);
+                        break;
+                    case DEADLINE:
+                        String deadlineDate = "";
+                        if (flags.containsKey("by")) {
+                            deadlineDate = flags.get("by");
+                        }
+                        list[count] = new Task(mainCmd[1], deadlineDate);
+                        break;
+                    case EVENT:
+                        String fromTime = "";
+                        String toTime = "";
+                        if (flags.containsKey("from")) {
+                            fromTime = flags.get("from");
+                        }
+                        if (flags.containsKey("to")) {
+                            toTime = flags.get("to");
+                        }
+                        list[count] = new Task(mainCmd[1], fromTime, toTime);
+                        break;
+                }
 
                 String text = "____________________________________________________________\n" +
-                        "added: " +
-                        input +
-                        "\n" +
+                        "Got it. I've added this task:\n" +
+                        list[count].toString() +
+                        "\nNow you have " + (count + 1) + " tasks in the list.\n" +
                         "____________________________________________________________\n";
                 System.out.println(text);
+
+                count++;
             }
         }
 
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,10 +1,30 @@
 public class Task {
     protected String description;
     protected boolean isDone;
+    protected TaskType type;
+    protected String deadlineDate;
+    protected String fromTime;
+    protected String toTime;
 
     public Task(String description) {
         this.description = description;
         this.isDone = false;
+        this.type = TaskType.TODO;
+    }
+
+    public Task(String description, String date) {
+        this.description = description;
+        this.isDone = false;
+        this.type = TaskType.DEADLINE;
+        this.deadlineDate = date;
+    }
+
+    public Task(String description, String from, String to) {
+        this.description = description;
+        this.isDone = false;
+        this.type = TaskType.EVENT;
+        this.fromTime = from;
+        this.toTime = to;
     }
 
     public String getStatusIcon() {
@@ -21,5 +41,20 @@ public class Task {
 
     public void markAsUndone() {
         isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        switch (type) {
+            case TODO:
+                return "[T][" + this.getStatusIcon() + "] " + description;
+            case DEADLINE:
+                return "[D][" + this.getStatusIcon() + "] " + description +
+                        " (by: " + deadlineDate + ")";
+            case EVENT:
+                return "[E][" + this.getStatusIcon() + "] " + description +
+                        " (from: " + fromTime + " to: " + toTime + ")";
+        }
+        return "";
     }
 }
