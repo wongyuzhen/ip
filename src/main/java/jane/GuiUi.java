4f6d450dbf9f3d00edece212c7a9d582c6651c1c
wongyuzhen <wongyuzhenn@gmail.com>
2025-09-04 13:24:41 +0800

    Add back accidentally deleted code

--- src/main/java/jane/GuiUi.java
+++ src/main/java/jane/GuiUi.java
@@ -30,4 +30,44 @@ public class GuiUi extends Ui {
     public void showBye() {
         appendLines("Bye. Hope to see you again soon!");
     }
+
+    @Override
+    public void showAdded(Task t, int size) {
+        buf.append("Got it. I've added this task:\n")
+            .append(t).append("\nNow you have ").append(size)
+            .append(" tasks in the list.\n");
+    }
+
+    @Override
+    public void showRemoved(Task t, int size) {
+        buf.append("Noted. I've removed this task:\n")
+            .append(t).append("\nNow you have ").append(size)
+            .append(" tasks in the list.\n");
+    }
+
+    @Override
+    public void showMarked(Task t, boolean done) {
+        buf.append(done
+                    ? "Nice! I've marked this task as done:\n"
+                    : "OK, I've marked this task as not done yet:\n")
+            .append(t).append("\n");
+    }
+
+    @Override
+    public void showList(TaskList tasks) {
+        buf.append("Here are the tasks in your list:\n");
+        for (int i = 0; i < tasks.size(); i++) {
+            buf.append(i + 1).append(". ").append(tasks.get(i)).append("\n");
+        }
+    }
+
+    @Override
+    public void showFoundTasks(TaskList tasks) {
+        buf.append("Here are the matching tasks in your list:\n");
+        for (int i = 0; i < tasks.size(); i++) {
+            buf.append(i + 1).append(".").append(tasks.get(i)).append("\n");
+        }
+    }
+
+    // readCommand() not used in GUI
 }
